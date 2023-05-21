034969d6695e57ab3c486f6043270defcf013cda
Nedyalko Draganov <nedyalkodraganov90@gmail.com>
2023-05-22 00:22:30 +0100

    Create a footer for the layout

---
+++ components/layout/footer/footer.jsx
@@ -0,0 +1,9 @@
+import classes from "./footer.module.css";
+
+export default function Footer() {
+  return (
+    <footer className={classes.footer}>
+      <p>Copyright 2023</p>
+    </footer>
+  );
+}
--- components/layout/layout.jsx
+++ components/layout/layout.jsx
@@ -1,11 +1,13 @@
 import { Fragment } from "react";
-import Navigation from "../header/navigation";
+import Navigation from "./header/navigation";
+import Footer from "./footer/footer";
 
 export default function Layout(props) {
   return (
     <Fragment>
       <Navigation />
       <main>{props.children}</main>
+      <Footer />
     </Fragment>
   );
 }
