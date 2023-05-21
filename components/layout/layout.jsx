53243f6682a523349fbc3874927991b1ea61776f
Nedyalko Draganov <nedyalkodraganov90@gmail.com>
2023-05-22 00:10:07 +0100

    Create a layout wrapper for the app

---
+++ components/layout/layout.jsx
@@ -0,0 +1,9 @@
+import { Fragment } from "react";
+
+export default function Layout(props) {
+  return (
+    <Fragment>
+      <main>{props.children}</main>
+    </Fragment>
+  );
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,14 +1,15 @@
 import Head from "next/head";
+import { Fragment } from "react";
 
 export default function Home() {
   return (
-    <>
+    <Fragment>
       <Head>
         <title>Event Guide</title>
         <meta name="description" content="" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-    </>
+    </Fragment>
   );
 }
