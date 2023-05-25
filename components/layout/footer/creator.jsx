c1935b059db0e51beb8f66d03621afb3e73bd6ae
Nedyalko Draganov <nedyalkodraganov90@gmail.com>
2023-05-26 00:26:29 +0100

    Install react icons and create creator component

---
+++ components/layout/footer/creator.jsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import { AiOutlineLinkedin } from "react-icons/ai";
+import { AiOutlineGithub } from "react-icons/ai";
+
+import classes from "./creator.module.css";
+
+export default function Creator() {
+  const linkedHref = "https://www.linkedin.com/in/nedyalko-draganov-12958a235/";
+  const gitHref = "https://github.com/NDraganov";
+  const currentYear = new Date().getFullYear();
+  return (
+    <div>
+      <div className={classes.section}>
+        <Link href={linkedHref} title="LinkedIn" target="_blank">
+          <AiOutlineLinkedin />
+        </Link>
+        <Link href={gitHref} title="GitHub" target="_blank">
+          <AiOutlineGithub />
+        </Link>
+      </div>
+      <div>
+        <p>Nedyalko Draganov</p>
+      </div>
+      <div className={classes.section}>
+        <p>
+          &copy;
+          <span>{currentYear}</span>
+        </p>
+      </div>
+    </div>
+  );
+}
--- components/layout/footer/footer.jsx
+++ components/layout/footer/footer.jsx
@@ -1,9 +1,10 @@
+import Creator from "./creator";
 import classes from "./footer.module.css";
 
 export default function Footer() {
   return (
     <footer className={classes.footer}>
-      <p>Copyright 2023</p>
+      <Creator />
     </footer>
   );
 }
