9d66e51d5d345a052d0fd86e18e9a9d36ea907ba
Nedyalko Draganov <nedyalkodraganov90@gmail.com>
2023-05-30 00:31:37 +0100

    Add styles to event search by title component

--- components/events-search/events-search-title.jsx
+++ components/events-search/events-search-title.jsx
@@ -15,17 +15,15 @@ export default function EventsSearchTitle(props) {
   return (
     <section>
       <form className={classes.formTitle} onSubmit={onSubmitHandler}>
-        <div className={classes.controls}>
-          <div className={classes.control}>
-            <label htmlFor="title">Title</label>
-            <select name="" id="title" ref={titleRef}>
-              {eventsTitle.map((event) => (
-                <option value={event.id} key={event.id}>
-                  {event.title}
-                </option>
-              ))}
-            </select>
-          </div>
+        <div className={classes.control}>
+          <label htmlFor="title">Title</label>
+          <select name="" id="title" ref={titleRef}>
+            {eventsTitle.map((event) => (
+              <option value={event.id} key={event.id}>
+                {event.title}
+              </option>
+            ))}
+          </select>
         </div>
         <Button text={"Find Events"} />
       </form>
