86bb46f6b39765f76283d79d0e808517560b4b35
Nedyalko Draganov <nedyalkodraganov90@gmail.com>
2023-06-03 20:31:37 +0100

    Add options and styles to the date form

--- components/events-search/events-search-date.jsx
+++ components/events-search/events-search-date.jsx
@@ -15,15 +15,32 @@ export default function EventsSearchDate(props) {
   };
   return (
     <section>
-      <form onSubmit={onSubmitDateHandler}>
+      <form className={classes.formDate} onSubmit={onSubmitDateHandler}>
         <div className={classes.controls}>
           <div className={classes.control}>
-            <label htmlFor="month"></label>
-            <select name="month" id="month" ref={monthRef}></select>
+            <label htmlFor="month">Month</label>
+            <select name="month" id="month" ref={monthRef}>
+              <option value="1">January</option>
+              <option value="2">February</option>
+              <option value="3">March</option>
+              <option value="4">April</option>
+              <option value="5">May</option>
+              <option value="6">June</option>
+              <option value="7">July</option>
+              <option value="8">August</option>
+              <option value="9">September</option>
+              <option value="10">October</option>
+              <option value="11">November</option>
+              <option value="12">December</option>
+            </select>
           </div>
           <div className={classes.control}>
-            <label htmlFor="year"></label>
-            <select name="year" id="year" ref={yearRef}></select>
+            <label htmlFor="year">Year</label>
+            <select name="year" id="year" ref={yearRef}>
+              <option value="2023">2023</option>
+              <option value="2024">2024</option>
+              <option value="2025">2025</option>
+            </select>
           </div>
         </div>
         <Button text={"Find Events"} />
